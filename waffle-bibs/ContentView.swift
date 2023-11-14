d43e9368660369fa117df2c89bae30025ee14060
hooni <jhlee909090@gmail.com>
2023-11-14 09:51:57 +0900

    레포 연결

---
+++ waffle-bibs/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  waffle-bibs
+//
+//  Created by 이지훈 on 2023/11/02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text(" Hell world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+    /////fsdfasdfsdafasdfasdfsadfsdafsda
+}
---
+++ waffle-bibs/waffle_bibsApp.swift
@@ -0,0 +1,17 @@
+//
+//  waffle_bibsApp.swift
+//  waffle-bibs
+//
+//  Created by 이지훈 on 2023/11/02.
+//
+
+import SwiftUI
+
+@main
+struct waffle_bibsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
