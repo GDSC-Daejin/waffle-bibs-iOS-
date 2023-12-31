e01bc59219afc550058628f50d775b495b068735
hooni <jhlee909090@gmail.com>
2023-12-31 18:02:29 +0900

    내부 삽입

--- waffle-bibs/AssimentView.swift
+++ waffle-bibs/AssimentView.swift
@@ -6,165 +6,115 @@
 
 import SwiftUI
 import Alamofire
-
 struct AssimentView: View {
-    
     var data: GridItemModel
-    @Environment(\.presentationMode) var presentationMode // 환경 변수 추가
+    @Environment(\.presentationMode) var presentationMode
     
-    @State private var items: [String] = Array(repeating: "", count: 8) // 8개의 비어 있는 항목으로 초기화
+    @State private var items: [String] = Array(repeating: "", count: 8)
+    @State private var editingIndex: Int? = nil
     @State private var newItem: String = ""
-    @State private var showAlert = false
-    @State private var showNewItemView = false
     
     
     var body: some View {
-        ZStack {
-            VStack(alignment: .leading) {
-                // Header UI
-                headerView
-                
-                // List and New Item UI
-                listAndNewItemView
-            }
-            .navigationBarHidden(true) // 기본 네비게이션 바 숨김
-            
-            // 새 항목 뷰 추가
-            if showNewItemView {
-                NewItemView(isPresented: $showNewItemView, newItem: $newItem, items: $items)
-                    .frame(maxWidth: 300, maxHeight: 20)
-                    .background(Color.white)
-                    .cornerRadius(12)
-                    .shadow(radius: 10)
-            }
-        }
-    }
-    
-    
-    
-    //MARK: - Header 추가
-    
-    // Header View
+          ZStack {
+              VStack(alignment: .leading) {
+                  headerView
+                  ScrollView {
+                      VStack(spacing: 10) {
+                          ForEach(0..<items.count, id: \.self) { index in
+                              ZStack {
+                                  if editingIndex == index {
+                                      TextField("새 항목", text: $items[index])
+                                          .textFieldStyle(RoundedBorderTextFieldStyle())
+                                          .onSubmit {
+                                              editingIndex = nil
+                                          }
+                                  } else {
+                                      Text(items[index].isEmpty ? "비어있는 항목" : items[index])
+                                  }
+                              }
+                              .frame(height: 120)
+                              .frame(maxWidth: .infinity)
+                              .background(Color("CustomBlue"))
+                              .cornerRadius(8)
+                              .swipeActions(edge: .leading, allowsFullSwipe: false) {
+                                  Button(role: .destructive) {
+                                      removeItem(at: index)
+                                  } label: {
+                                      Label("삭제", systemImage: "trash")
+                                  }
+                              }
+                          }
+                      }
+                      .padding(.horizontal)
+                  }
+                  .background(Color.white)
+              }
+              .navigationBarHidden(true)
+          }
+      }
+
     var headerView: some View {
         HStack {
-            
             backButton
             HeadText
             Spacer()
-          //  deleteButton
             addButton
         }
         .padding()
     }
-    
+
     var backButton: some View {
         Button(action: {
-            self.presentationMode.wrappedValue.dismiss() // 뒤로 가기 기능
+            self.presentationMode.wrappedValue.dismiss()
         }) {
-            Image("backBtn")
-                .resizable()
-                .frame(width: 20, height: 20)
+            Image("backBtn").resizable().frame(width: 20, height: 20)
         }
     }
     
     var HeadText: some View {
         Text("Assiment")
-            .frame(alignment: .leading)
             .font(.custom("Inter", size: 30))
             .bold()
             .foregroundColor(Color("CustomFont"))
-        
     }
     
-//    var deleteButton: some View {
-//        Button(action: {
-//            // 오른쪽 첫 번째 버튼 액션
-//        }) {
-//            Image("deleteBtn")
-//                .resizable()
-//                .frame(width: 30, height: 30)
-//        }
-//    }
-//
     var addButton: some View {
         Button(action: {
-            showNewItemView = true
+            if let firstEmptyIndex = items.firstIndex(where: { $0.isEmpty }) {
+                editingIndex = firstEmptyIndex
+            }
         }) {
             ZStack {
-                Image("addBack")
-                    .resizable()
-                    .frame(width: 23, height: 23)
-                Image("addFore")
-                    .resizable()
-                    .frame(width: 17, height: 17)
+                Image("addBack").resizable().frame(width: 23, height: 23)
+                Image("addFore").resizable().frame(width: 17, height: 17)
             }
         }
     }
+    
     //MARK: -
+
     
-    // List and New Item View
-    var listAndNewItemView: some View {
-        VStack {
-            TextField("새 항목", text: $newItem)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding()
-            
-            ScrollView {
-                VStack(spacing: 10) {
-                    ForEach(0..<items.count, id: \.self) { index in
-                        Text(items[index].isEmpty ? "" : items[index])
-                            .frame(height: 120)
-                            .frame(maxWidth: .infinity)
-                            .background(Color("CustomBlue"))
-                            .cornerRadius(8)
-                    }
-                }
-                .padding(.horizontal)
-            }
-            .background(Color.white)
-        }
-        .background(Color.white)
+    func removeItem(at index: Int) {
+        items.remove(at: index)
+        items.append("") // 맨 끝에 새로운 빈 항목 추가
     }
-    
+
     func addNewItem() {
-           if !newItem.isEmpty {
-               // 첫 번째 빈 항목을 찾아서 새 항목으로 업데이트합니다.
-               if let firstEmptyIndex = items.firstIndex(where: { $0.isEmpty }) {
-                   items[firstEmptyIndex] = newItem
-                   newItem = "" // 입력 필드 초기화
-               }
-           }
-       }
-}
-
-
-struct NewItemView: View {
-    @Binding var isPresented: Bool
-    @Binding var newItem: String
-    @Binding var items: [String]
-    
-    var body: some View {
-        VStack {
-            TextField("새 항목", text: $newItem)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding()
-            Button("추가하기") {
-                if !newItem.isEmpty {
-                    items.append(newItem)
-                    newItem = ""
-                    isPresented = false
-                }
+        if !newItem.isEmpty {
+            if let firstEmptyIndex = items.firstIndex(where: { $0.isEmpty }) {
+                items[firstEmptyIndex] = newItem
+                newItem = ""
             }
-            .padding()
         }
     }
 }
+    
 
 
 struct AssimentView_Previews: PreviewProvider {
     static var previews: some View {
-        let sampleItem = GridItemModel(imageName: "assiment", labelText: "Assignment")
-        AssimentView(data: sampleItem)
+        AssimentView(data: GridItemModel(imageName: "assiment", labelText: "Assignment"))
     }
 }
 
